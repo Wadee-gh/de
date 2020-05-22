025da8f104e7b1b0797cfb02a030a3cb91573351
root <root@ip-172-31-30-219.us-west-2.compute.internal>
2020-05-22 19:19:31 +0000

    databases dump

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 mkdir -p tmp/runtime tmp/assets
 chmod 777 tmp/runtime/
 chmod 777 tmp/assets/
-sudo apt install php-mbstring
-sudo apt-get install php-xml
+sudo apt install 
+sudo apt php-mbstring 
+sudo apt php-xml 
+sudo apt php-mysql
 sudo service apache2 restart
